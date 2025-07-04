efeab387af1ed5e5ebaf6eadc037be057d1ef989
egor-white <channela082@gmail.com>
2025-07-04 11:54:07 +0300

    change ver

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.cherret.zaprett"
         minSdk = 30
         targetSdk = 35
-        versionCode = 12
-        versionName = "2.0"
+        versionCode = 13
+        versionName = "2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         ndk {
