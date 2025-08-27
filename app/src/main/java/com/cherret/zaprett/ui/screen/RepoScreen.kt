fd06b6c75b9e23dfb43bf8acc4caa0b60f97cf47
CherretGit <petlindmitriy9@gmail.com>
2025-08-28 02:35:33 +0700

    navigationBarsPadding in repo screen

--- app/src/main/java/com/cherret/zaprett/ui/screen/RepoScreen.kt
+++ app/src/main/java/com/cherret/zaprett/ui/screen/RepoScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -176,7 +177,7 @@ fun RepoScreen(navController: NavController, viewModel: BaseRepoViewModel) {
             ) {
                 LazyColumn(
                     contentPadding = paddingValues,
-                    modifier = Modifier.fillMaxSize()
+                    modifier = Modifier.navigationBarsPadding().fillMaxSize()
                 ) {
                     if (hostLists.isEmpty()) {
                         item {
