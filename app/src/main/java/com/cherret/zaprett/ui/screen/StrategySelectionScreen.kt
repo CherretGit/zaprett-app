efb60d0c93165a39f57c12160c5ab5854d9c373c
CherretGit <petlindmitriy9@gmail.com>
2025-12-08 16:02:44 +0700

    fix shadow

--- app/src/main/java/com/cherret/zaprett/ui/screen/StrategySelectionScreen.kt
+++ app/src/main/java/com/cherret/zaprett/ui/screen/StrategySelectionScreen.kt
@@ -11,9 +11,12 @@ import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -25,7 +28,6 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
@@ -46,7 +48,9 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
@@ -149,8 +153,13 @@ fun StrategySelectionScreen(navController: NavController, vpnLauncher: ActivityR
         snackbarHost = { SnackbarHost(snackbarHostState) },
         content = { paddingValues ->
             LazyColumn (
+                contentPadding = PaddingValues(
+                    top = paddingValues.calculateTopPadding(),
+                    bottom = paddingValues.calculateBottomPadding() + 40.dp
+                ),
                 modifier = Modifier
-                    .padding(paddingValues)
+                    .navigationBarsPadding()
+                    .fillMaxSize()
             ) {
                 item {
                     Row (
